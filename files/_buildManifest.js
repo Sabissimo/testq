8666cf5ebefdb18ea3f52da281e914f5f10f5836
Sabissimo <sabanadze@gmail.com>
2022-07-16 21:09:40 +0400

    Add files via upload

---
+++ files/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/837-4a9c09f5c5206d47.js","static/chunks/pages/index-efc0390cbb451c75.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
